226379c7049b76e9e3fa9058d4281a70249e0370
RealCodeStream <github2023JP@gmail.com>
2024-06-29 12:04:43 -0500

    conexión exitosa a la database

--- server/index.js
+++ server/index.js
@@ -5,23 +5,23 @@ const app = express();
 const cors = require('cors');
 
 
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*'); 
     res.header(
         'Access-Control-Allow-Headers', 'Origin,X-CSRF-Token, X-Requested-With, Accept-Version, Content-Length, Content-MD5, Content-Type, Accept, Date, X-Api-Version, Authorization, X-Auth-Token');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS,PATCH');
     res.header('Access-Control-Allow-Credentials', 'true');
     next();
-  });
+  });*/
 const startServer = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/semestral");
+        await mongoose.connect("mongodb://localhost:27017/semestralds5");
         console.log('Connected to DB');
         //await createInitialAdmin();
 
         app.use(cors());
         app.use(express.json());
-        app.use(routes);
+        //app.use(routes);
         
         // Iniciar servidor
         app.listen(9000, () => {
